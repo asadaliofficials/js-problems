6a31d500cd7d095c2956ad0b3c38981042d21b10
Asad Ali <message.asadali@gmail.com>
2024-12-06 10:49:00 +0500

    Add find minimum value in array.js

---
+++ find minimum value in array.js
@@ -0,0 +1,7 @@
+// Find the minimum value in an array
+let numbers = [5, 2, 8, 1, 9, 3];
+
+// Using Math.min with spread operator
+let minValue = Math.min(...numbers);
+
+console.log(minValue); // Output: 1
