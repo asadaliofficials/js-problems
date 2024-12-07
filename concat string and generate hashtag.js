3c1d590b3b5a46a23dc580b56860e9b37e56a883
Asad Ali <message.asadali@gmail.com>
2024-12-07 10:33:00 +0500

    Add concat string and generate hashtag.js

---
+++ concat string and generate hashtag.js
@@ -0,0 +1,10 @@
+// You have given a string and you have to Capitalize its first character of each word and then concat them and then generate a hashtag.
+
+let string = 'The quick brown fox jumped over the lazy DOG';
+let words = string.split(' ');
+let CapitalizedWords = words.map(word => {
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+});
+
+let hashtag = '#' + CapitalizedWords.join('');
+console.log(hashtag);
