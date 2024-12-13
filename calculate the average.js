6fe11d8cfc718eaa5c7979e68b40d15abac4c392
Asad Ali <message.asadali@gmail.com>
2024-12-13 11:15:00 +0500

    Remove calculate the average.js

--- calculate the average.js
+++
@@ -1,7 +0,0 @@
-// Calculate the average of numbers in an array
-let numbers = [10, 20, 30, 40, 50];
-
-let sum = numbers.reduce((acc, num) => acc + num, 0);
-let average = sum / numbers.length;
-
-console.log(average); // Output: 30
