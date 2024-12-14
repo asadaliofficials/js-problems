5c9c6872c1b4d7a70676a223059cdc156ca4944b
Asad Ali <message.asadali@gmail.com>
2024-12-14 11:57:00 +0500

    Add 3. check the words are palindrome or not.js

---
+++ 3. check the words are palindrome or not.js
@@ -0,0 +1,16 @@
+// Write a function to determine whether a given string is
+// a palindrome or not. A palindrome is a word, phrase,
+// number, or other sequence of characters that reads the same
+// forward and backward, ignoring spaces, punctuation, and
+// capitalization.
+
+function isPalindrome(string) {
+  let rawCharacters = string.toLowerCase().split('');
+  let characters = rawCharacters.filter(c => c !== ' ' && c !== ',');
+  let original = characters.join('');
+  let reverse = characters.reverse().join('');
+  return original === reverse;
+}
+console.log(isPalindrome('A man, a plan, a canal,Panama')); // Output: true
+console.log(isPalindrome('racecar')); // Output: true
+console.log(isPalindrome('hello')); // Output: false
