def3fff8fa982c71cdd88781916d2cc0d8762763
Asad Ali <message.asadali@gmail.com>
2024-12-17 09:55:00 +0500

    Add filter even numbers.js

--- Filter Even Numbers.js
+++ Filter Even Numbers.js
@@ -1,21 +1,6 @@
-// function filterEvenNumbers(array){
-//   filteredArray = []
-//   array.forEach(element => {
-//     if((element % 2) == 0){
-//       filteredArray.push(element)
-//     }
-//   });
-//   console.log(filteredArray)
-// }
+// Filter even numbers from an array
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-// filterEvenNumbers([1, 2, 3, 4, 5, 6]); // [2, 4, 6]
-// filterEvenNumbers([7, 8, 9, 10, 11]); // [8, 10]
+let evenNumbers = numbers.filter(num => num % 2 === 0);
 
-
-function filterEvenNumbers(array) {
-  let filteredArray = array.filter(element => element % 2 === 0);
-  console.log(filteredArray);
-} 
-
-filterEvenNumbers([1, 2, 3, 4, 5, 6]); // [2, 4, 6]
-filterEvenNumbers([7, 8, 9, 10, 11]); // [8, 10]
+console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
