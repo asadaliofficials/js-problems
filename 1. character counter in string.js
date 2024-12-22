e9d0db75fd3e82e781a6ab5d9450b35da207ada5
Asad Ali <message.asadali@gmail.com>
2024-12-22 10:13:00 +0500

    Remove 1. character counter in string.js

--- 1. character counter in string.js
+++
@@ -1,9 +0,0 @@
-// Count the occurrence of each character in a string
-let string = "Hello World";
-let charCount = {};
-
-for (let char of string) {
-  charCount[char] = (charCount[char] || 0) + 1;
-}
-
-console.log(charCount);
