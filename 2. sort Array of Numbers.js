454c1ad6df137cbc4f83ff965248f66d67900d05
Asad Ali <message.asadali@gmail.com>
2024-12-24 10:54:00 +0500

    Add 2. sort Array of Numbers.js

---
+++ 2. sort Array of Numbers.js
@@ -0,0 +1,7 @@
+// Sort an array of numbers in ascending order
+let numbers = [5, 2, 8, 1, 9, 3];
+
+// Using sort method
+let sortedNumbers = numbers.sort((a, b) => a - b);
+
+console.log(sortedNumbers);
