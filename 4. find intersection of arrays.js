6753487b5779cedbf70128c5b118b6317afbcf71
Asad Ali <message.asadali@gmail.com>
2025-01-23 13:49:00 +0500

    Remove 4. find intersection of arrays.js

--- 4. find intersection of arrays.js
+++
@@ -1,9 +0,0 @@
-// Find intersection of two arrays
-function findIntersection(arr1, arr2) {
-  return arr1.filter(value => arr2.includes(value));
-}
-
-let array1 = [1, 2, 3, 4, 5];
-let array2 = [3, 4, 5, 6, 7];
-
-console.log(findIntersection(array1, array2)); // Output: [3, 4, 5]
