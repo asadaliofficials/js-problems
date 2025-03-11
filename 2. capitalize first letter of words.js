701dee285519c784bcf970a6b978129758c52992
Asad Ali <message.asadali@gmail.com>
2025-03-11 10:34:00 +0500

    Add 2. capitalize first letter of words.js

---
+++ 2. capitalize first letter of words.js
@@ -0,0 +1,9 @@
+// Capitalize the first letter of each word in a sentence
+function capitalizeWords(sentence) {
+  return sentence
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+
+console.log(capitalizeWords("hello world")); // Output: "Hello World"
