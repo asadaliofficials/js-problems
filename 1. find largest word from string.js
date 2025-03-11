d8319ed0c16adf024072d2f529c35237712ebf8a
Asad Ali <message.asadali@gmail.com>
2025-03-11 09:46:00 +0500

    Add 1. find largest word from string.js

--- 1. find largest word from string.js
+++ 1. find largest word from string.js
@@ -3,14 +3,9 @@ let string = 'The quick brown fox jumped over the lazy dog';
 
 let words = string.split(' ');
 
-// let longestWord = words.sort((a, b) => {
-// 	return a.length - b.length;
-// });
-// console.log(longestWord.at(-1));
-
 let longestWord = words.reduce(
-	(accumulator, currentWord) =>
-		accumulator.length < currentWord.length ? currentWord : accumulator,
-	''
+  (accumulator, currentWord) =>
+    accumulator.length < currentWord.length ? currentWord : accumulator,
+  ''
 );
 console.log(longestWord);
