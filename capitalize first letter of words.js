0cdb9f00aa0d13b032378ec64ac4c01df33fe1df
Asad Ali <message.asadali@gmail.com>
2025-11-03 10:31:00 +0500

    Remove capitalize first letter of words.js

--- capitalize first letter of words.js
+++
@@ -1,9 +0,0 @@
-// Capitalize the first letter of each word in a sentence
-function capitalizeWords(sentence) {
-  return sentence
-    .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-    .join(' ');
-}
-
-console.log(capitalizeWords("hello world")); // Output: "Hello World"
