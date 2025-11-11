7ae60a6a42d0e580dd0053dfeefc70ea5aba6828
Asad Ali <message.asadali@gmail.com>
2025-11-11 10:42:00 +0500

    Remove find the max number from the array.js

--- find the max number from the array.js
+++
@@ -1,7 +0,0 @@
-// Find the maximum number from an array
-let numbers = [5, 12, 8, 3, 19, 7, 1];
-
-// Using Math.max with spread operator
-let maxNumber = Math.max(...numbers);
-
-console.log(maxNumber);
