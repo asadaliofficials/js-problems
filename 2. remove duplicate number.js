b8141db9ad84a4b8314e330edc22a74e6cf99b93
Asad Ali <message.asadali@gmail.com>
2025-11-15 11:57:00 +0500

    Remove 2. remove duplicate number.js

--- 2. remove duplicate number.js
+++
@@ -1,7 +0,0 @@
-// Remove duplicate numbers from an array
-let numbers = [1, 2, 3, 2, 4, 1, 5, 3];
-
-// Using Set to remove duplicates
-let uniqueNumbers = [...new Set(numbers)];
-
-console.log(uniqueNumbers); // Output: [1, 2, 3, 4, 5]
