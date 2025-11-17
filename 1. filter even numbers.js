f13c50db8d84e71dbdf74458f9f2e6b4ba3205dd
Asad Ali <message.asadali@gmail.com>
2025-11-17 10:30:00 +0500

    Remove 1. filter even numbers.js

--- 1. filter even numbers.js
+++
@@ -1,6 +0,0 @@
-// Filter even numbers from an array
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-let evenNumbers = numbers.filter(num => num % 2 === 0);
-
-console.log(evenNumbers); // Output: [2, 4, 6, 8, 10]
