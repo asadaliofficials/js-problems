cd5dd47abced16332660bcb41b1f8b50ad3b8a7b
Asad Ali <message.asadali@gmail.com>
2025-11-20 10:14:00 +0500

    Remove sort Array of Numbers.js

--- sort Array of Numbers.js
+++
@@ -1,7 +0,0 @@
-// Sort an array of numbers in ascending order
-let numbers = [5, 2, 8, 1, 9, 3];
-
-// Using sort method
-let sortedNumbers = numbers.sort((a, b) => a - b);
-
-console.log(sortedNumbers);
